471106383dc592a6bfa826bff0b655257b712f96
Tim Peters <zomg.tim@gmail.com>
2018-12-06 19:40:18 +1030

    Allow running specific day/part

--- run.sh
+++ run.sh
@@ -2,9 +2,13 @@
 
 cargo test && cargo build --release || exit 1
 
-for d in $(ls -1 input/day*.txt | grep -o '[[:digit:]]\+' | sort -n); do
-    echo -n "Day $d Part One: "
-    ./target/release/adventofcode-2018 $d 1
-    echo -n "Day $d Part Two: "
-    ./target/release/adventofcode-2018 $d 2
-done
+if [ ! -z "$1" ]; then
+    ./target/release/adventofcode-2018 $@
+else
+    for d in $(ls -1 input/day*.txt | grep -o '[[:digit:]]\+' | sort -n); do
+        echo -n "Day $d Part One: "
+        ./target/release/adventofcode-2018 $d 1
+        echo -n "Day $d Part Two: "
+        ./target/release/adventofcode-2018 $d 2
+    done
+fi
