64bfc195a102bea6e57279e2e793bc62cb948545
Tim Peters <zomg.tim@gmail.com>
2018-12-06 18:20:48 +1030

    Add day 5 part 2

--- src/day5.rs
+++ src/day5.rs
@@ -1,5 +1,9 @@
 pub fn run(part: i32, input: &str) {
-    println!("{}", react(input).len());
+    if part == 1 {
+        println!("{}", react(input).len());
+    } else {
+        println!("{}", shortest_polymer(input).len());
+    }
 }
 
 fn react(input: &str) -> String {
@@ -27,6 +31,26 @@ fn react(input: &str) -> String {
     }
 }
 
+fn remove_types(input: &str, uc: char) -> String {
+    let lc = uc.to_ascii_lowercase();
+    input.chars().filter(|&c| c != uc && c != lc).collect()
+}
+
+fn shortest_polymer(input: &str) -> String {
+    let mut shortest: Option<String> = None;
+    for c in (b'A' .. b'Z').map(|c| c as char) {
+        let r = react(&remove_types(input, c));
+        shortest = match shortest {
+            Some(s) => Some(if r.len() < s.len() { r } else { s }),
+            None => Some(r)
+        }
+    }
+    match shortest {
+        Some(s) => s,
+        None => "".to_string()
+    }
+}
+
 fn is_pair(a: u8, b: u8) -> bool {
     a >= 97 && a <= 122 && b == a - 32 || a >= 65 && a <= 90 && b == a + 32
 }
@@ -40,4 +64,21 @@ fn test_react() {
     assert_eq!("dabCBAcaDA", react("dabAcCaCBAcCcaDA"));
     assert_eq!(10, react("dabAcCaCBAcCcaDA").len());
     assert_eq!(10, react("dabAcCaCBAcCcaDA\n").len());
+}
+
+#[test]
+fn test_remove_type() {
+    assert_eq!("dbcCCBcCcD", remove_types("dabAcCaCBAcCcaDA", 'A'));
+    assert_eq!("daAcCaCAcCcaDA", remove_types("dabAcCaCBAcCcaDA", 'B'));
+    assert_eq!("dabAaBAaDA", remove_types("dabAcCaCBAcCcaDA", 'C'));
+    assert_eq!("abAcCaCBAcCcaA", remove_types("dabAcCaCBAcCcaDA", 'D'));
+}
+
+#[test]
+fn test_shortest_polymer() {
+    assert_eq!("dbCBcD", react(&remove_types("dabAcCaCBAcCcaDA", 'A')));
+    assert_eq!("daCAcaDA", react(&remove_types("dabAcCaCBAcCcaDA", 'B')));
+    assert_eq!("daDA", react(&remove_types("dabAcCaCBAcCcaDA", 'C')));
+    assert_eq!("abCBAc", react(&remove_types("dabAcCaCBAcCcaDA", 'D')));
+    assert_eq!("daDA", shortest_polymer("dabAcCaCBAcCcaDA"));
 }
