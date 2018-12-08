887c76037e1d83e381a07244f109ec761c0065b3
Tim Peters <zomg.tim@gmail.com>
2018-12-08 17:18:18 +1030

    Add day 8 part 2

--- src/day8.rs
+++ src/day8.rs
@@ -1,10 +1,11 @@
 use super::{Part,Part::*};
 
 pub fn run(part: Part, input: &str) {
-    match part {
-        One => println!("{}", metadata_sum(&parse_input(input))),
-        Two => println!()
-    }
+    let input = parse_input(input);
+    println!("{}", match part {
+        One => metadata_sum(&input),
+        Two => value(&input)
+    });
 }
 
 fn parse_input(input: &str) -> Vec<usize> {
@@ -12,35 +13,64 @@ fn parse_input(input: &str) -> Vec<usize> {
 }
 
 fn metadata_sum(input: &[usize]) -> usize {
-    metadata_sum_child(input).0
+    parse_tree(input).sum
 }
 
-// returns (sum, size) indicating the metadata sum of node starting at `start`
-// and how many numbers were used up
-fn metadata_sum_child(input: &[usize]) -> (usize, usize) {
-    let children = input[0];
-    let metadata = input[1];
-    let mut index = 2;
-    let mut sum = 0;
-    for _ in 0..children {
-        let (child_sum, child_used) = metadata_sum_child(&input[index..]);
-        sum += child_sum;
-        index += child_used;
+fn value(input: &[usize]) -> usize {
+    parse_tree(input).value
+}
+
+struct Info {
+    used: usize,
+    sum: usize,
+    value: usize
+}
+
+fn parse_tree(input: &[usize]) -> Info {
+    let num_children = input[0];
+    let num_metadata = input[1];
+    let mut info = Info {
+        used: 2,
+        sum: 0,
+        value: 0
+    };
+    let mut children = vec![];
+
+    for _ in 0..num_children {
+        let child = parse_tree(&input[info.used..]);
+        info.sum += child.sum;
+        info.used += child.used;
+        children.push(child);
     }
-    for i in index..index + metadata {
-        sum += input[i];
+
+    for i in input.iter().skip(info.used).take(num_metadata) {
+        info.sum += i;
+        info.used += 1;
+        if num_children == 0 {
+            info.value += i;
+        } else if i - 1 < children.len() {
+            info.value += children[i - 1].value;
+        }
     }
-    (sum, index + metadata)
+
+    info
 }
 
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     fn test_input() -> &'static str {
         "2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2\n"
     }
 
     #[test]
     fn test_metadata_sum() {
-        assert_eq!(138, super::metadata_sum(&super::parse_input(test_input())));
+        assert_eq!(138, metadata_sum(&parse_input(test_input())));
+    }
+
+    #[test]
+    fn test_value() {
+        assert_eq!(66, value(&parse_input(test_input())));
     }
 }
