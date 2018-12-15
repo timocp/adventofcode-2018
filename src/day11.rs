cf16cd33ad9b3fce179b80cb764fe6879e6cc3a1
Tim Peters <zomg.tim@gmail.com>
2018-12-15 18:14:16 +1030

    Add day 11 part 2

--- src/day11.rs
+++ src/day11.rs
@@ -1,40 +1,105 @@
 use super::{Part, Part::*};
+use std::cmp;
+use std::fmt;
 
 pub fn run(part: Part, input: &str) {
     let grid_serial: usize = input.trim().parse().unwrap();
     match part {
-        One => println!("{:?}", largest_power(grid_serial)),
-        Two => println!(),
+        One => println!("{}", largest_power(grid_serial)),
+        Two => println!("{}", max_powers(grid_serial)),
+    }
+}
+
+#[derive(Copy, Clone, PartialEq, Debug)]
+struct Answer {
+    x: usize,
+    y: usize,
+    size: usize,
+    value: i32,
+}
+
+impl fmt::Display for Answer {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{},{},{}", self.x, self.y, self.size)
     }
 }
 
 // returns (x,y) of the top-left of the largest 3x3 grid
-fn largest_power(grid_serial: usize) -> (usize, usize) {
+fn largest_power(grid_serial: usize) -> Answer {
     let grid = init_grid(grid_serial);
-    let mut max_value = -46;
-    let mut max_position = (0, 0);
-    for x in 0..298 {
-        for y in 0..298 {
+    let mut max = Answer {
+        x: 0,
+        y: 0,
+        size: 3,
+        value: -6 * (3 * 3) as i32,
+    };
+    for x in 0..=(300 - 3) {
+        for y in 0..=(300 - 3) {
             let mut total = 0;
             for xx in 0..3 {
                 for yy in 0..3 {
-                    total += grid[(x + xx) * 300 + y + yy];
+                    total += grid[x + xx][y + yy];
                 }
             }
-            if total > max_value {
-                max_value = total;
-                max_position = (x + 1, y + 1);
+            if total > max.value {
+                max.value = total;
+                max.x = x + 1;
+                max.y = y + 1;
             }
         }
     }
-    max_position
+    max
 }
 
-fn init_grid(grid_serial: usize) -> Vec<i32> {
-    let mut grid = vec![0; 300 * 300];
+fn max_powers(grid_serial: usize) -> Answer {
+    let grid = init_grid(grid_serial);
+    let sums = summed_area(&grid);
+    let mut answer = Answer {
+        x: 0,
+        y: 0,
+        size: 0,
+        value: 300 * 300 * -5,
+    };
+
+    for x in 0..299 {
+        for y in 0..299 {
+            for d in 1..(300 - cmp::max(x, y) - 1) {
+                let total = sums[x + d][y + d] + sums[x][y] - sums[x + d][y] - sums[x][y + d];
+                if total > answer.value {
+                    answer.x = x + 2;
+                    answer.y = y + 2;
+                    answer.size = d;
+                    answer.value = total;
+                }
+            }
+        }
+    }
+    answer
+}
+
+fn summed_area(grid: &Vec<Vec<i32>>) -> Vec<Vec<i32>> {
+    let mut sums = vec![vec![0; 300]; 300];
     for x in 0..300 {
         for y in 0..300 {
-            grid[x * 300 + y] = power_level(x + 1, y + 1, grid_serial);
+            if x == 0 && y == 0 {
+                sums[x][y] = grid[x][y];
+            } else if x == 0 {
+                sums[x][y] = grid[x][y] + sums[x][y - 1];
+            } else if y == 0 {
+                sums[x][y] = grid[x][y] + sums[x - 1][y];
+            } else {
+                sums[x][y] = grid[x][y] + sums[x][y - 1] + sums[x - 1][y] - sums[x - 1][y - 1];
+            }
+        }
+    }
+    sums
+}
+
+fn init_grid(grid_serial: usize) -> Vec<Vec<i32>> {
+    let mut grid = vec![vec![0; 300]; 300];
+    for x in 0..300 {
+        for y in 0..300 {
+            grid[x][y] = power_level(x + 1, y + 1, grid_serial);
         }
     }
     grid
@@ -56,6 +121,45 @@ fn test_power_level() {
 
 #[test]
 fn test_largest_power() {
-    assert_eq!((33, 45), largest_power(18));
-    assert_eq!((21, 61), largest_power(42));
+    assert_eq!(
+        Answer {
+            x: 33,
+            y: 45,
+            size: 3,
+            value: 29
+        },
+        largest_power(18)
+    );
+    assert_eq!(
+        Answer {
+            x: 21,
+            y: 61,
+            size: 3,
+            value: 30
+        },
+        largest_power(42)
+    );
+}
+
+#[ignore]
+#[test]
+fn test_max_powers() {
+    assert_eq!(
+        Answer {
+            x: 90,
+            y: 269,
+            size: 16,
+            value: 113
+        },
+        max_powers(18)
+    );
+    assert_eq!(
+        Answer {
+            x: 232,
+            y: 251,
+            size: 12,
+            value: 119
+        },
+        max_powers(42)
+    );
 }
